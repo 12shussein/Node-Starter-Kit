2d4b191ffd218b57b57f910fbc2e337c33105cf3
Abdirahim Hussein <drahimhussein@gmail.com>
2022-12-10 12:26:46 +0000

    third step completed

--- server.js
+++ server.js
@@ -2,15 +2,19 @@ const express = require('express');
 const app = express();
 
 // define a route for the GET request
-app.get('/', (req, res) => {
-  // return a list of users in JSON format
+app.get('/users', (req, res) => {
+  // extract the query string from the request
+  const search = req.query.search;
+
+  // return a list of users that match the search term in JSON format
   res.json([
     { id: 1, name: 'John Doe' },
     { id: 2, name: 'Jane Smith' }
-  ]);
+  ].filter(user => user.name.includes(search)));
 });
 
 // start the server
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
 });
+
